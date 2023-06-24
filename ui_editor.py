043baef24857aa324641477665a91bc1f913c683
MaddoxRiley314159265 <125212972+MaddoxRiley314159265@users.noreply.github.com>
2023-06-24 13:38:18 -0500

    Add files via upload

--- ui_editor.py
+++ ui_editor.py
@@ -8,7 +8,7 @@ pygame.init()
 
 init_menus((1000, 800))
 
-ui_config.menus.extend([Menu([Button(c(500, 700), "center", c(200, 400), next_menu, border_img=bg_img), Label(c(0, 600), "nw", c(400, 100)), Text(c(100,800), "se", 50, "Hello World"), Entry(c(500, 800), "nw", c(300, 300), 50, clamp_text=False)], bg_img=bg_img, bg_img_name=bg_img_name), 
+ui_config.menus.extend([Menu([Button(c(500, 700), "center", c(200, 400), next_menu, border_img_name=bg_img_name), Label(c(0, 600), "nw", c(400, 100)), Text(c(500,800), "se", 50, "Hello World"), Entry(c(500, 800), "nw", c(300, 300), 50, clamp_text=False)], bg_img_name=bg_img_name), 
             Menu([Label(c(200, 200), "nw", c(200, 100), text="Welcome to menu 2"), Button(c(500, 500), "center", c(300, 100), prev_menu, text="If we could turn back time")], bg_col=(200, 200, 100))])
     
 
